f83903986a5cfe9f4adab76929e240a91ec08988
Paschalis Natsidis <pnatsidis@hotmail.com>
2019-09-13 15:28:35 +0300

    Update phy2nexus.sh

--- phy2nexus.sh
+++ phy2nexus.sh
@@ -1,6 +1,6 @@
 FILENAME=$1
 
-echo "ToNexus FromFile=$FILENAME ToFile=$FILENAME.nexus DataType=RestSite Interleave=Yes;
+echo "ToNexus FromFile=$FILENAME ToFile=$FILENAME.nexus DataType=Protein Interleave=Yes;
 execute $FILENAME.nexus;
 export File=$FILENAME.interleaved.nexus Format=Nexus Interleaved=Yes CharsPerLine=99990 Replace=Yes;
 quit;" > convert_to_nexus.paup
