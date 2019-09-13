4ea54a4a07a66ebc031ae56ce6bf3545f27acf1e
Paschalis Natsidis <pnatsidis@hotmail.com>
2019-09-13 15:24:32 +0300

    Create phy2nexus.sh

---
+++ phy2nexus.sh
@@ -0,0 +1,10 @@
+FILENAME=$1
+
+echo "ToNexus FromFile=$FILENAME ToFile=$FILENAME.nexus DataType=RestSite Interleave=Yes;
+execute $FILENAME.nexus;
+export File=$FILENAME.interleaved.nexus Format=Nexus Interleaved=Yes CharsPerLine=99990 Replace=Yes;
+quit;" > convert_to_nexus.paup
+
+paup4a166_centos64 convert_to_nexus.paup
+
+rm $1.nexus
